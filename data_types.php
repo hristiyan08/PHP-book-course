952e5c21af949c63201084e6f7371abd3dce3e8e
Hristiyan <hristianjordanov08@gmail.com>
2024-12-28 21:29:51 +0200

    Add file

---
+++ data_types.php
@@ -0,0 +1,35 @@
+<?php
+
+//Integer
+$num1 = 5;
+$num2 = 10;
+$num2 = 3;
+echo $num1 + $num2 . "<br>";
+echo $num1 - $num2 . "<br>";
+
+//String
+$text1 = "Hello";
+$text2 = "Peter";
+$symbol = "!";
+echo "$text1 $text2$symbol" . "<br>";
+
+//Double
+$doubleNum1 = 16.2;
+$doubleNum2 = 2;
+echo $doubleNum1 / $doubleNum2 . "<br>";
+
+//Arrays
+$ints = [1,2,3,4,5,6,7,8,9];
+echo $ints[1] * $ints[2] . "<br>";
+
+$strings = ["PHP", "Java", "Python"];
+echo $strings[0] . "<br>";
+
+
+//Constants
+define('num1', 3);
+echo num1 . "<br>";
+
+define('text3', "Hello World!");
+echo text3 . "<br>";
+
