afe773baebe84562b497cf3f7c9d9d63df54e9c8
Hristiyan <hristianjordanov08@gmail.com>
2024-12-29 22:27:04 +0200

    Add files and changes

--- loops.php
+++ loops.php
@@ -7,8 +7,8 @@ for ($i = 1; $i <= 10; $i++) {  // $i -> control variable in loop. The loop will
     echo $i . "\n"; //from 1 to 10 (inc)
 }
 
-for ($i = 1; $i < 10; $i++) { //from 1 to 9 (not to 10)
-    echo $i . "\n";
+for ($i = 1; $i < 10; $i++) {
+    echo $i . "\n";//from 1 to 9 (not to 10)
 }
 
 for ($i = 1, $j = 1; $i < 10; $i++, $j++) { // for loop with multiple control variables
